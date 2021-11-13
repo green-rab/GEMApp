7bc16f664279efaa458dc8108a6f30721448e433
Markus Schmidt <markus-schmidt@email.de>
2021-11-13 11:28:18 +0100

    edited dummy_service for more robustness

--- userServices/dummy_service.cpp
+++ userServices/dummy_service.cpp
@@ -28,5 +28,13 @@ T_dummy_service::~T_dummy_service() {
     ## T_dummy_service :: execute() - Run the service ##
 **/
 void T_dummy_service::execute(t_GEMApp_data &data) {
-    data.GPIO_12 = (data.GPIO_05 & data.GPIO_06) | data.GPIO_13;
+    data.GPIO_12 = false;
+
+    if(data.GPIO_05 == true && data.GPIO_06 == true) {
+        data.GPIO_12 = true;
+    }
+    
+    if(data.GPIO_13 == true) {
+        data.GPIO_12 = true;
+    }
 }
